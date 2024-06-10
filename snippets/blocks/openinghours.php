e5c432709e642da5a59023b3729fb5fd1f2c19bb
Marc <marc@zephir.ch>
2024-06-10 08:23:24 +0000

    Fix issue with date format; 0.0.6

--- snippets/blocks/openinghours.php
+++ snippets/blocks/openinghours.php
@@ -23,10 +23,10 @@ $o = new OpeninghoursModel();
     <strong>
         <?php if (!$openinghour->isDefault()): ?>
             <?php
-                $start = $openinghour->getStartDate('E, d.M.Y');
+                $start = $openinghour->getStartDate('E, d.MM.Y');
                 $start = substr_replace($start, '', strpos($start, '.'), 1);
 
-                $end = $openinghour->getEndDate('E, d.M.Y');
+                $end = $openinghour->getEndDate('E, d.MM.Y');
                 $end = substr_replace($end, '', strpos($end, '.'), 1);
             ?>
 
